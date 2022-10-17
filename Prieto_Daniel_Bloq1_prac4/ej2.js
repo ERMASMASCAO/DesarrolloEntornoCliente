a5bc786c9d035956ab9ac4e84ad5c3ee81cbd2e2
ERMASMASCAO <danielcaminasadm@gmail.com>
2022-10-17 12:05:33 +0200

    eventos

---
+++ Prieto_Daniel_Bloq1_prac4/ej2.js
@@ -0,0 +1,10 @@
+function grande(element) {
+    element.style.fontSize = '16pt';
+
+}
+function pequeno(element) {
+   element.style.fontSize = '12pt';
+
+}
+document.getElementById("parrafo").onmouseover(grande);
+document.getElementById("parrafo").onmouseover(pequeno);
